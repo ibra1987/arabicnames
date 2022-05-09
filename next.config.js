442de2024227b7991d70c6f2490e830d7a9140bf
brahim driouch <brahim.driouch1987@glmail.com>
2022-05-09 16:12:45 -0400

    fixconfig

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ const nextConfig = {
   images: {
     domains: ["images.ctfassets.net"],
   },
-  env: {
-    CONTENTFUL_SPACEID: process.env.CONTENTFUL_SPACEID,
-    CONTENTFUL_DELIVERY_KEY: process.env.CONTENTFUL_DELIVERY_KEY,
-  },
 };
 
 module.exports = nextConfig;
