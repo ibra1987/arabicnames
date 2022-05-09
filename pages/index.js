100685ed16f96d297d89c614282d029af4be203d
brahim driouch <brahim.driouch1987@glmail.com>
2022-05-09 19:20:09 -0400

    refctor

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ import Name from "../models/Name";
 import LatestPosts from "../components/Home/LatestPosts";
 import Subscribe from "../components/Home/Subscribe";
 import RandomNames from "../components/Home/RandomNames";
-import getAll from "../utils/contentful/getAll";
 import LeaderBoard from "../components/ads/LeaderBoard";
+import { contentful } from "../utils/contentful/config";
 
 const Home = ({ posts, randomNames }) => {
   return (
@@ -32,13 +32,15 @@ const Home = ({ posts, randomNames }) => {
 export default Home;
 
 export async function getServerSideProps() {
-  const res = await getAll("blog");
-  const randomNames = await Name.aggregate([{ $sample: { size: 3 } }]);
+  const res = await contentful.getEntries({
+    content_type: "blog",
+  });
+  //const randomNames = await Name.aggregate([{ $sample: { size: 3 } }]);
 
   return {
     props: {
       posts: res.items,
-      randomNames: JSON.stringify(randomNames),
+      // randomNames: JSON.stringify(randomNames),
     },
   };
 }
