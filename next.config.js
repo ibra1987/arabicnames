ce3e4b0cd75d1d18baeb4930697241ca25a1035f
brahim driouch <brahim.driouch1987@glmail.com>
2022-05-09 15:56:02 -0400

    fixvercelerroraddenvvariablestonextconfig

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
   images: {
     domains: ["images.ctfassets.net"],
   },
+  env: {
+    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACEID,
+    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_DELIVERY_KEY,
+  },
 };
 
 module.exports = nextConfig;
