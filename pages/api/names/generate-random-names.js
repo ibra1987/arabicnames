6b1c5deecabc1d85aa7d6af44d57b2e9f27b8f45
brahim driouch <brahim.driouch1987@glmail.com>
2022-05-10 16:46:28 -0400

    cors

--- pages/api/names/generate-random-names.js
+++ pages/api/names/generate-random-names.js
@@ -1,11 +1,13 @@
 import connection from "../../../database/DBConnect";
 import RandomName from "../../../models/RandomName";
 import Name from "../../../models/Name";
+import defineCors from "../../../utils/http/defineCors";
 
 async function handler(req, res) {
   if (req.method !== "POST") {
     return res.redirect(307, "/");
   }
+  await defineCors();
   const { token } = req.body;
   if (!token) return res.redirect(307, "/");
   const SECRET = process.env.RANDOM_NAMES_SECRET;
