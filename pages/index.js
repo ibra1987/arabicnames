bccc746fd50c8bc5e1f9573a3ed27648a2412a95
brahim driouch <brahim.driouch1987@glmail.com>
2022-05-24 22:00:35 -0400

    googleverification

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ import connection from "../database/DBConnect";
 import { toast } from "react-toastify";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import Head from "next/head";
 
 const Home = ({ posts, randomNames }) => {
   const router = useRouter();
@@ -21,6 +22,19 @@ const Home = ({ posts, randomNames }) => {
   }, [router, sub]);
   return (
     <section className="w-full relative flex flex-col justify-start items-center">
+      <Head>
+        <title>Find An Arabic Girl Name Four Your Baby Girl</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="choose an arabic girl name four your baby girl. You can browse our full list of beautiful and cute girl names with their meanings."
+        />
+        <meta name="robots" content="index, follow" />
+        <meta
+          name="google-site-verification"
+          content="Ch9-qg1xL6F6DTvV4WTOyFT5qDPMArEoUoOkJvhCJzs"
+        />
+      </Head>
       <Hero />
       <LeaderBoard />
 
