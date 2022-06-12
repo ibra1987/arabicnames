74623955ff83ae2f2c62856d57f66a52a1353f13
brahim driouch <brahim.driouch1987@glmail.com>
2022-06-12 13:43:31 -0400

    54

--- layouts/Default.js
+++ layouts/Default.js
@@ -4,7 +4,7 @@ import Header from "../components/Shared/Header";
 export default function Layout({ children }) {
   return (
     <>
-      <main className=" max-w-11/12 md:w-3/5 mx-auto popins flex flex-col justify-start items-center  ">
+      <main className=" w-11/12 md:w-4/5 mx-auto popins flex flex-col justify-start items-center  ">
         <Header />
         {children}
         <Footer />
