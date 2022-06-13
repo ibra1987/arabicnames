fbdb28fecbb643783fc1d80f0434f012e888fc96
brahim driouch <brahim.driouch1987@glmail.com>
2022-06-13 13:49:04 -0400

    54

--- components/Home/Hero.js
+++ components/Home/Hero.js
@@ -10,7 +10,7 @@ const Hero = () => {
   const SearchButtonStyles =
     " w-11/12 md:w-2/5 py-2 outline-none bg-pink-500 mx-4 tracking-widest text-white cursor-pointer hover:bg-white hover:text-pink-500 font-bold hover:border hover:border-pink-500 ease-in duration-300  rounded-md popins";
   const ExploreButtonStyles =
-    " w-full py-2 bg-white mx-4 ouline-none border tracking-widest border-pink-500 font-bold text-pink-500 cursor-pointer hover:bg-pink-500 hover:text-white ease-in duration-300   rounded-md popins";
+    " w-11/12 md:w-2/5 py-2 bg-white mx-4 ouline-none border tracking-widest border-pink-500 font-bold text-pink-500 cursor-pointer hover:bg-pink-500 hover:text-white ease-in duration-300   rounded-md popins";
   const InputStyle =
     " focus:border-pink-200 text-gray-800 md:w-3/5 my-4  mx-2 p-2 outline-none border rounded border-gray-200";
   const onChange = (e) => {
