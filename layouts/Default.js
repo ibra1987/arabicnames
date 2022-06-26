78a3190946659a743d014e14287422da7197fea8
brahim driouch <brahim.driouch1987@glmail.com>
2022-06-25 22:43:16 -0400

    jh

--- layouts/Default.js
+++ layouts/Default.js
@@ -7,12 +7,7 @@ export default function Layout({ children }) {
       <main className=" w-11/12 bg-white border shadow-md mx-auto popins flex flex-col justify-start items-center  ">
         <Header />
         {children}
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MLTR7GT"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-          }}
-        />
+
         <Footer />
       </main>
     </>
